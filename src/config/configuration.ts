3fd968c0d5f3dba15613a40bb4c92ab79e2ab0c3
gene9831 <pacify.98@gmail.com>
2021-09-05 03:28:46 +0800

    🔧更换config位置

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { utilities, WinstonModuleOptions } from 'nest-winston';
-import { join } from 'path';
+import { dirname, join } from 'path';
 import * as winston from 'winston';
 import { validate } from './config.validation';
 
@@ -12,7 +12,7 @@ const yamlConfig = () => {
   // 在这里进行验证
   return validate(
     yaml.load(
-      readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+      readFileSync(join(dirname(__dirname), YAML_CONFIG_FILENAME), 'utf8'),
     ) as Record<string, any>,
   );
 };
